eba87378bc88aa5ec6571ea65a255e0bd096736c
Tu <679639@pdsb.net>
2023-06-06 12:21:19 -0400

    asd

--- src/components/Dashboard/NoteGroup.js
+++ src/components/Dashboard/NoteGroup.js
@@ -1,12 +1,17 @@
-import { useLocation } from "react-router";
+import { useParams } from "react-router";
+import { useEffect } from "react";
 
 export default function NoteGroup() {
-  const location = useLocation();
+  const params = useParams();
+
+  useEffect(() => {
+    console.log(params)
+  }.[])
 
   return (
-    <div>
-      <h1>Note Group</h1>
-      {location.pathname}
+    <div className="dashboard-page">
+      <h1>NoteGroup</h1>
+      {JSON.stringify(params)}
     </div>
   );
 }
