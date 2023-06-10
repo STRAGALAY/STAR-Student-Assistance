d131e805bc942fea84085e98064e708659bd256d
gosqib <69024184+gosqib@users.noreply.github.com>
2023-06-10 13:53:38 -0400

    fix the position of the  goals popup button

--- src/App.js
+++ src/App.js
@@ -58,18 +58,6 @@ function Home() {
         </div>
       </div>
 
-      {/* goals popup */}
-      <a href="/goals">
-        <button className="goals-button">
-          <img
-            className="goalspic"
-            src="https://img.freepik.com/free-vector/positive-tiny-people-sitting-walking-near-huge-target-with-arrow-isolated-flat-vector-illustration-cartoon-business-team-achieving-goal-aim-marketing-strategy-achievement-concept_74855-10139.jpg?t=st=1685989671~exp=1685990271~hmac=cef3442898edaa479d462947089e4c953c0bf35d8da91a6f34374cfacdd2fe08"
-            width="85"
-            height="85"
-          />
-        </button>
-      </a>
-
       <div className="app">
         <div className="homepage-panel-1">
           <div className="side right-side">
--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { NotesProvider } from "./contexts/NotesProvider";
 
+// import "./App.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -24,6 +26,18 @@ root.render(
     </BrowserRouter>
 
     {/* <Footer /> */}
+
+    {/* goals popup */}
+    <a href="/goals">
+      <button className="goals-button">
+        <img
+          className="goalspic"
+          src="https://img.freepik.com/free-vector/positive-tiny-people-sitting-walking-near-huge-target-with-arrow-isolated-flat-vector-illustration-cartoon-business-team-achieving-goal-aim-marketing-strategy-achievement-concept_74855-10139.jpg?t=st=1685989671~exp=1685990271~hmac=cef3442898edaa479d462947089e4c953c0bf35d8da91a6f34374cfacdd2fe08"
+          width="85"
+          height="85"
+        />
+      </button>
+    </a>
   </React.StrictMode>
 );
 
